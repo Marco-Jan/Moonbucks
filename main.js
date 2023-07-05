c06eaf297ea267e2a91e057f6dc109bc63081d2e
Iron <marco.jan@gmx.de>
2023-07-05 11:50:23 +0200

    init commit

--- main.js
+++ main.js
@@ -145,6 +145,28 @@ function createTag(parent_node, tag_node, id_name, class_name, content) {
 
 //create multiple tags
 
+function createMultiTags(parent_node, tag_node, num, list, menu) {
+  for (let i = 0; i < num; i++) {
+    if (menu) {
+      createTag(
+        parent_node,
+        tag_node,
+        null,
+        list[i],
+        '<a href="#' + list[i].replace(/ /g, "-") + '">' + list[i] + "</a>"
+      );
+    } else {
+      createTag(
+        parent_node,
+        tag_node,
+        null,
+        list[i].replace(/ /g, "_"),
+        list[i]
+      );
+    }
+  }
+}
+
 //create header
 function createHeader() {
   const header = createTag(null, "header");
